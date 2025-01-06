9055ca9d0675b15615a7930d550445bfb20dfd7e
yassjok <yassine.buissnes@gmail.com>
2025-01-06 23:44:03 +0100

    Utilis.h

---
+++ Utilis.h
@@ -0,0 +1,19 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PROJECT_NAME "Stock Management System"
+void add_product();
+void edit_product();
+void recherche_produit();
+void afficher_liste_produit ();
+void Tri_produit();
+void removeProductByName();
+void login();
+void isuerexist();
+void addusertofile();
+
+
+#endif
